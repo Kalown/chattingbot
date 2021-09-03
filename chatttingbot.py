4c0beaf83f6d685213c4e2996cbc670cb9b8094a
Kalown <86701829+Kalown@users.noreply.github.com>
2021-09-03 10:43:56 +0530

    Update chatttingbot.py

--- chatttingbot.py
+++ chatttingbot.py
@@ -13,28 +13,31 @@ def greet (lang):
 greet(a)
 print("I am kalown")
 print("I was created on 2021")
-
+name=input("what is your name ?")
 """ Asking about age"""
 
 def year(age):
     if age<=16:
-        print('you are a kid dumbass')
-    elif age>=17:
-        print('wasssup buddy')
-    elif age>=27:
-        print('HELLO MASTER')
+        print(name,'you are a kid')
+    elif age<=25:
+        print('wasssup',name)
+    elif age>=26:
+        print('HELLO MASTER',name)
     else:pass
 b=int(input("what year was u born "))
 print()
 print("what you was born on ",b," Really")
 c=2021
-if b>=2021:
-    print('You must be joking!')
-else:
-    d=c-b
-    print(d)
-    year(d)
-print()
+
+try :
+  if b>=2021:
+     print('You must be joking!')
+  else:pass
+
+except :
+ globalsd=c-b
+ print(d)
+ year(d)
 
 """After age equation"""
 
@@ -42,8 +45,23 @@ age=str(d)
 if age<="16":
         print('so what you watch cartoon or tv shows\n')
         e=input(">")
-        if e=="cartoon":
-         f=input(print("what cartoon do you watch? \n"))   #level 2
+        if e=="cartoon" or e>=23:
+            if e=="cartoon":
+
+             cartoon_0=input("what cartoon do you watch?\n") #level 2
+            cartoon_1=print("ohh so u like watching ",cartoon_0,"that's cool")
+            fav_char=input('who is your favourate character\n')
+            print("ohh",fav_char,"that's coool, u are really cool kid\n")
+            fam_0=input("so who is in your family?\n")
+            fam_0_1=print("ohh so you have ",fam_0,"in your family\n")
+            fam_1=input("who is your favourate member of the family?\n")
+            fam_1_1=print("ohhk so",fam_1,"is your favourate\n")
+            school_0=input("so what school you go?\n")
+            school_0_1=print(school_0,"hmm ok what class you in?\n")
+            school_0_1_1=input(">")
+            school_0_2=print("ohh so you in",school_0_1_1,"\n")
+            print("ohhk buddy now i have to go , take some rest good bye!!!\n")
+            bye=input("")
 
         elif e=="tv shows":
             g=input(print("what tv show you like to watch \n")) #level 2
@@ -56,3 +74,9 @@ elif age>="27":
   print("okk! so what's new going on ?")
 
 else:pass
+
+#Report on the subject intracted
+zero=0
+z=int(input("Enter the password"))
+if zero ==(2016-z):
+    print("The name of the subject is",)
